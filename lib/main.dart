234783222bb025753746f80d5cd912f88a398817
tamathecxder <yudistiraeka.pratama012@gmail.com>
2022-10-16 17:22:58 +0700

    Membungkus body dengan kolom dan expanded widget untuk section favorites

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:chat_ui/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main(List<String> args) {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -14,10 +14,12 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Chat UI",
       theme: ThemeData(
+        primaryColor: Colors.red,
         colorScheme: ColorScheme.fromSwatch().copyWith(
           secondary: Color(0xFFFEF9EB),
           primary: Colors.red,
         ),
+        fontFamily: "Inter",
       ),
       home: const HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -11,6 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
       appBar: AppBar(
         leading: IconButton(
           icon: Icon(Icons.menu),
@@ -22,7 +23,7 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         title: const Text(
           "Chats",
-          style: TextStyle(fontSize: 28.0, fontWeight: FontWeight.bold),
+          style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
         ),
         actions: [
           IconButton(
@@ -34,7 +35,20 @@ class _HomeScreenState extends State<HomeScreen> {
         ],
       ),
       body: Column(
-        children: [CategorySelector()],
+        children: [
+          CategorySelector(),
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Theme.of(context).colorScheme.secondary,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30.0),
+                  topRight: Radius.circular(30.0),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
@@ -50,11 +64,40 @@ class CategorySelector extends StatefulWidget {
 }
 
 class _CategorySelectorState extends State<CategorySelector> {
+  int selectedIndex = 0;
+  final List<String> categories = ["Messages", "Online", "Groups", "Request"];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       height: 90.0,
-      color: Colors.grey,
+      color: Theme.of(context).primaryColor,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: categories.length,
+        itemBuilder: (context, index) {
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: 20.0, vertical: 30.0),
+              child: Text(
+                "${categories[index]}",
+                style: TextStyle(
+                    color:
+                        index == selectedIndex ? Colors.white : Colors.white60,
+                    fontWeight: FontWeight.w600,
+                    fontSize: 20.0,
+                    letterSpacing: 0.5),
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
